db4e9d7c38ea20ddbd6afc7adb1c38674e9f3add
Nathan Lapinski <n4t311@gmail.com>
2018-04-16 21:10:01 +0900

    more initial styling

--- APP-Start/src/app/app.module.ts
+++ APP-Start/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule
